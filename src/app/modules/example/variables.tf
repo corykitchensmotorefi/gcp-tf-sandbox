fb93c73a4f54a4cad2cf2473b2269ad758e8fa8b
Cory Kitchens <cory.kitchens@motorefi.com>
2021-06-04 15:57:12 -0700

    Basic scaffolding

---
+++ src/app/modules/example/variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  description = "bucket name (required)"
+  type        = string
+}
+
+variable "uniform_bucket_level_access" {
+  description = "uniform_bucket_level_access"
+  type        = bool
+  default     = false
+}
---
+++ src/app/stacks/demo/variables.tf
@@ -0,0 +1,5 @@
+variable "uniform_bucket_level_access" {
+  description = "uniform_bucket_level_access"
+  type        = bool
+  default     = false
+}
